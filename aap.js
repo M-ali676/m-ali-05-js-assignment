47d910e79eb3fd8592fa8b20f7110e42da628336
M-ali676 <solangimuhammadali676@gmail.com>
2025-08-14 15:33:54 +0500

    Create aap.js

---
+++ aap.js
@@ -0,0 +1,47 @@
+//____________________ Q-01 Declare a variable called ________________________//
+
+// var username; 
+
+//_______________ Q-02 Myname & asign _____________________________//
+
+// var myName = "Muhammad Ali Solangi";
+
+//___________________ Q-03 Write script _______________________________//
+
+// var massage;
+// massage = "Hello World";
+// alert(massage)
+
+//__________________ Q-04 Students Bio data _______________________________//
+
+// var studentsName = "Ali";
+// var studentAge = "20 years old";
+// var studentCourse = "Web and Mobile App Development";
+// alert(studentsName);
+// alert(studentAge);
+// alert(studentCourse);
+
+// _____________________________ Q-05 Display alert ________________//
+
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza);
+
+//______________________Q-06 Email address and alert ____________________//
+
+// var email = "solangimuhammadali676@gmail.com";
+// alert("My email address is " + email);
+
+//_________________Q-07 Book massage alert ________________________//
+
+// var book = "A smarter way to laern Javascript";
+// alert("I am trying to learn from the Book " + book);
+
+//____________________Q-08 Display this in browser through JS ______________//
+
+// alert("Yah! I can write HTML content through JavaScript");
+
+//_____________________ Q-09 Show design ___________________//
+
+// var design = "_____@@@@@___";
+// alert(design);
+// alert(design);
